83b424bb1d291d610c88ae4504036908c9a18897
JeffY1710 <jeff.yabas@gmail.com>
2024-04-19 07:27:04 +0200

    add scripts

---
+++ setup-front.bash
@@ -0,0 +1,17 @@
+apt update
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.bashrc
+
+nvm install v20.10.0
+
+apt install npm -y
+
+apt install git -y
+
+git clone https://github.com/lucasTrswl/Securite_front.git
+
+cd /Securite_front
+
+npm i
